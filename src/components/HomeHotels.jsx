0e52e3b1312898779a65e1e482db5485fd5c2190
saisandeepkoritala <saisandeep.koritala@gmail.com>
2023-11-09 23:25:46 -0600

    second commit

--- src/components/HomeHotels.jsx
+++ src/components/HomeHotels.jsx
@@ -6,18 +6,31 @@ import { useEffect } from "react";
 import axios from "axios";
 import {useDispatch,useSelector} from "react-redux";
 import { setHotels } from "../store/index";
+import {Link} from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
 
 const HomeHotels = () => {
 
   const dispatch = useDispatch();
-  const fetched= useSelector((store)=>store.form.data)
-  console.log(fetched)
+  const hotels = useSelector((store)=>store.form.data)
+  console.log(hotels)
 
   useEffect(()=>{
     axios.get("http://localhost:5000/getData")
     .then((res)=>dispatch(setHotels(res.data.data.hotels)))
     .catch((err)=>console.log("error",err))
   },[])
+
+  const rendered = hotels.map((hotel)=>{
+    const srcimg = hotel.main_photo_url.replace("square60","square300")
+    return (
+    <Link key={hotel.hotel_id} className="hotel">
+        <img src={srcimg} alt="" />
+        <p>{hotel.hotel_name}</p>
+        <ReactStars count={5} size={24} value={Math.round(hotel.review_score/2)} activeColor="#ffd700" edit={false}/>
+        <p>Review - {hotel.review_score}/10</p>
+    </Link>
+  )})
   
   return (
     <div className='homehotels'>
@@ -27,12 +40,12 @@ const HomeHotels = () => {
         <img src={pic3} alt="" />
         <img src={pic4} alt="" />
       </div>
-      <div>
-        <p>Best Choices</p>
-        <p>Popular Residencies</p>
+      <div className="holder">
+        <h2>Best Choices</h2>
+        <h3>Popular Residencies</h3>
       </div>
-      <div>
-        {}
+      <div className="hotels">
+        {rendered}
       </div>
     </div>
   )
